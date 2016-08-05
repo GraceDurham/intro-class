62dfef814e311f36678626a0d6dc384f83a672a0
Grace <gracelee.durham@gmail.com>
2016-08-04 21:18:00 -0700

    loops

---
+++ while_fun_loops.py
@@ -0,0 +1,27 @@
+# count=1
+# while (count<21):
+# 	print count 
+# 	count = count + 1
+
+
+# count = 1
+# while (count < 21):
+# 	if count == 13:
+# 		print "hello"
+# 	else:
+# 		print count 
+# 	count = count + 1
+
+# count = 0 
+# while (count <=100):
+# 	if count % 10 ==0:
+# 		print count
+# 	count +=1
+
+fruits = ["apples", "oranges", "bananas"]
+
+index = 0
+
+while index < len(fruits):
+	print fruits [index]
+	index = index + 1
