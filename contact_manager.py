fcd969683e0a8b2ba07278f62767f23273235774
Grace Durham <gracelee.durham@gmail.com>
2016-08-25 21:36:41 -0700

    Add files via upload

--- contact_manager.py
+++ contact_manager.py
@@ -13,21 +13,27 @@ def main():
 
 	contact_list2=[]
 	# contat 1
-	first_name=raw_input ("Please enter a first name")
-	last_name= raw_input ("Please enter a last name")
-	mobile_number=raw_input ("Please enter a mobile number")
-	email=raw_input ("Please enter an email")
+	_break = None
+	while True:
+		_break = raw_input("press 2 to break")
+		print "let's make a contact list"
+		first_name=raw_input ("Please enter a first name")
+		last_name= raw_input ("Please enter a last name")
+		mobile_number=raw_input ("Please enter a mobile number")
+		email=raw_input ("Please enter an email")
 
-	contact_little= Contact(first_name, last_name, mobile = mobile_number, email = email)
-	contact_list2.append(contact_little)
-	#contact 2
-	# first_name="little" #raw_input ("Please enter a first name")
-	# last_name="finger" #raw_input ("Please enter a last name")
-	# mobile_number="4156666666"#raw_input ("Please enter a mobile number")
-	# email="little.finger@evil.com" #raw_input ("Please enter an email")
+		contact_little= Contact(first_name, last_name, mobile = mobile_number, email = email)
+		contact_list2.append(contact_little)
+		if _break == "2":
+			break
 
 	print contact_list2 
-	print contact_list2[0].__dict__
+
+	for obj in contact_list2:
+		print obj.__dict__
+
+
+
 
 main()
 
