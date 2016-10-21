5d442c82e9928b0cbbc9b76be91e80b9b28a9020
Grace Durham <gracelee.durham@gmail.com>
2016-10-20 21:34:56 -0700

    Add files via upload

--- BillCalculator1.py
+++ BillCalculator1.py
@@ -4,7 +4,7 @@ def calculate_tip(bill_amount,tip_percent):
     tip = bill_amount *(float(tip_percent)/100)
     return tip    
 
-print calculate_tip(20,10)
+# print calculate_tip(20,10)
 
 def calculate_total_bill(tip_amount, bill_amount):
     total_money=tip_amount + bill_amount
@@ -12,32 +12,34 @@ def calculate_total_bill(tip_amount, bill_amount):
 
 
 
-print calculate_total_bill(20,10)    
+# print calculate_total_bill(20,10)    
 
 def calculate_tip(bill_amount,tip_percent):
     tip = bill_amount *(float(tip_percent)/100)
     return tip    
 
-print calculate_tip(20,10)
+# print calculate_tip(20,10)
 
 def split_bill(bill_amount, num_people):
     split = bill_amount/num_people
     return split
 
-print split_bill(100,10)
+# print split_bill(100,10)
 
 def main():    
     print "Welcome to bill calculator"    
     original_bill_amount=float(raw_input("Please enter the original bill amount"))
     tip_percentage=int(raw_input("Please enter the tip percentage"))
 
-    choice_1 = int(raw_input("Please choose from choice 1 or 2 choice 1 is calculate tip")) 
-    if choice_1 == 1:
+    choice = int(raw_input("""Please enter 1 to calculate tip or
+                            enter 2 to split the bill""")) 
+    
+    if choice == 1:
 
         tip_amount=original_bill_amount * tip_percentage/100
         print tip_amount
-    choice_2= int(raw_input("Please enter number"))   
-    if choice_2==2:
+    
+    if choice==2:
         total_bill_amounts=int(raw_input("Please enter total bill amount")) 
         number_of_people_split=int(raw_input("How many people would you like to split the bill with "))
 
